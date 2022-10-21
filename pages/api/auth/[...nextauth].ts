d57eb7a328dd79a96b4e38b016fbaae4f6a3beff
kihwan.lee <kihwan.lee@lemonhc.com>
2022-10-21 16:42:06 +0900

    카카오 간편 로그인 기능 적용

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,18 @@
 import NextAuth from 'next-auth'
 import type { NextAuthOptions } from 'next-auth'
 import NaverProvider from "next-auth/providers/naver"
+import KakaoProvider from "next-auth/providers/kakao"
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     NaverProvider({
-        clientId: process.env.NAVER_CLIENT_ID,
-        clientSecret: process.env.NAVER_CLIENT_SECRET
+      clientId: process.env.NAVER_CLIENT_ID,
+      clientSecret: process.env.NAVER_CLIENT_SECRET
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET
     }),
     // ...add more providers here
   ],
