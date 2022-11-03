bc3c6c96de411bfcff278b9acb9980a12ad7c845
sungaee <sungaee121@naver.com>
2022-11-03 17:34:44 +0900

    주소로 좌표생성, 지도 클릭 시 좌표정보 표시

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,7 +1,7 @@
 import Script from 'next/script';
 import Header from '../components/common/Header'
 import Mapside from '../components/map/mapside'
-import { useEffect } from "react";
+import { FC, useEffect } from "react";
 
 interface MapProps {
   latitude: number;
@@ -14,14 +14,14 @@ declare global {
     }
   }
 
-function Map({ latitude, longitude }: MapProps) {
+
+const Map: FC<{address: string}> = ({ address }) => {
   useEffect(() => {
     const mapScript = document.createElement("script");
     const NEXT_PUBLIC_KAKAOMAP_KEY = process.env.NEXT_PUBLIC_KAKAOMAP_KEY;
     // const ps = new window.kakao.maps.services.Places();
     // ps.keywordSearch('입력 값', placesSearchCB);
     
-
     mapScript.async = true;
     mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&autoload=false&libraries=services`;
 
@@ -29,24 +29,75 @@ function Map({ latitude, longitude }: MapProps) {
 
     const onLoadKakaoMap = () => {
       window.kakao.maps.load(() => {
-        const container = document.getElementById("map");
-        const options = {
-          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-          lever: 3, //지도 확대레벨
-        };
-        const geocoder = new window.kakao.maps.services.Geocoder(); //// 해당 위치 값이 어딘지 알게해주는 역할
-        const map = new window.kakao.maps.Map(container, options);
-        const markerPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
-        const marker = new window.kakao.maps.Marker({
-          position: markerPosition,
-        });
-        marker.setMap(map);
-      });
+        
+
+        const geocoder = new window.kakao.maps.services.Geocoder(); // 주소-좌표 변환 객체를 생성
+
+        // 주소로 좌표를 검색
+        geocoder.addressSearch('서울시 금천구 가산디지털1로 145', function (result: any, status: any) {
+          if(status === window.kakao.maps.services.Status.OK){
+            var coords = new window.kakao.maps.LatLng(result[0].y, result[0].x)
+            //지도생성
+            const container = document.getElementById("map");
+            const options = {
+              center: coords,
+              lever: 3, //지도 확대레벨
+            };
+            // 지도생성
+            const map = new window.kakao.maps.Map(container, options);
+          
+            // 결과값으로 받은 위치를 마커로 표시
+            const marker = new window.kakao.maps.Marker({
+              map: map,
+              position: coords,
+            }) 
+            marker.setMap(map); 
+            
+            //지도에 클릭 이벤트 등록
+            window.kakao.maps.event.addListener(map, 'click', function(mouseEvent:any){
+              // 클릭한 위도, 경도 정보를 가져옴
+              var latlng = mouseEvent.latLng;
+
+              // 마커위치를 클릭한 위치로 옮김
+              marker.setPosition(latlng);
+            
+              var message = '위도: ' + latlng.getLat() + ' , ';
+              message += '경도: ' + latlng.getLng() + ' .';
+
+        
+              var infowindow = new window.kakao.maps.InfoWindow({
+                content: message
+            });
+              infowindow.open(map, marker);
+
+              // 지도의 중심을 결과값으로 받은 위치로 이동
+              map.setCenter(coords);
+            })
+
+
+          } else{ // 정상적으로 좌표검색 안될경우 디폴트 좌표로 검색
+            const container = document.getElementById("map");
+            const options = {
+              center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+              lever: 3, //지도 확대레벨
+            };
+            // 지도생성
+            const map = new window.kakao.maps.Map(container, options);
+            const marker = new window.kakao.maps.Marker({
+              map: map,
+              position: coords,
+            })
+            marker.setMap(map);
+          }
+        })
+
+
+      });//onLoadKakaoMap
     };
     mapScript.addEventListener("load", onLoadKakaoMap);
 
     return () => mapScript.removeEventListener("load", onLoadKakaoMap);
-  }, [latitude, longitude]);
+  }, [address]);
 
   return (
     
