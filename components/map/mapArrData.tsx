25f862f26b8b7d261cc24319ae5016fca75f75f0
sungaee <sungaee121@naver.com>
2022-11-16 10:22:40 +0900

    지도 리스트 파일분리

---
+++ components/map/mapArrData.tsx
@@ -0,0 +1,89 @@
+import Image from 'next/image'
+import HouseImage from '../../public/img/houseImg.jpeg';
+import HouseImage2 from '../../public/img/houseImg2.jpeg';
+import HouseImage3 from '../../public/img/houseImg3.jpeg';
+
+
+const mapArr = 
+        <div className="w-full flex-col flex growcursor-pointer">
+            <div className="flex grow items-center w-full border_bottom_line p-[25px] bg-white h-[200]}">
+                <div className="bg-slate-600 border-black border-2">
+                    <Image width={160} height={200} src={HouseImage} />
+                </div>
+                <div className="grow flex w-0 pl-5 ">
+                    <div>
+                        <h1>[전세] 24억 1000</h1>
+                        <p className='text-lg'>구로구 가산동</p>
+                        <p className='leading-6'>쓰리룸, 관리비 4만<br></br>혼자살기 좋은매물 준신축급<br></br>넓은1.5룸</p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex grow items-center w-full border_bottom_line p-[25px] bg-white h-[200]}">
+                <div className="bg-slate-600 border-black border-2">
+                    <Image height={200} width={160} src={HouseImage2} />
+                </div>
+                <div className="grow flex w-0 pl-5 ">
+                    <div>
+                        <h1>[매매] 4억 3000</h1>
+                        <p className='text-lg'>구로구 가산동</p>
+                        <p className='leading-6'>쓰리룸, 관리비 4만<br></br>혼자살기 좋은매물 준신축급<br></br>넓은1.5룸</p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex grow items-center w-full border_bottom_line p-[25px] bg-white h-[200]}">
+                <div className="bg-slate-600 border-black border-2">
+                    <Image height={200} width={160} src={HouseImage3} />
+                </div>
+                <div className="grow flex w-0 pl-5 ">
+                    <div>
+                        <h1>[월세] 500/34</h1>
+                        <p className='text-lg'>구로구 가산동</p>
+                        <p className='leading-6'>쓰리룸, 관리비 4만<br></br>혼자살기 좋은매물 준신축급<br></br>넓은1.5룸</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+      const mapArr2 =  
+      <div className="w-full flex-col flex growcursor-pointer ">
+        <div className="flex grow items-center w-full border_bottom_line p-[25px] bg-white h-[200]}">
+            <div className="bg-slate-600 border-black border-2">
+                <img height={200} width={160} src='/img/houseImg2.jpeg' />
+
+            </div>
+            <div className="grow flex w-0 pl-5 ">
+                <div>
+                    <h1>[매매] 4억 3000</h1>
+                    <p className='text-lg'>구로구 가산동</p>
+                    <p className='leading-6'>쓰리룸, 관리비 4만<br></br>혼자살기 좋은매물 준신축급<br></br>넓은1.5룸</p>
+                </div>
+            </div>
+        </div>
+        <div className="flex grow items-center w-full border_bottom_line p-[25px] bg-white h-[200]}">
+            <div className="bg-slate-600 border-black border-2">
+                <img height={200} width={160} src='/img/houseImg3.jpeg' />
+
+            </div>
+            <div className="grow flex w-0 pl-5 ">
+                <div>
+                    <h1>[전세] 2억 1000</h1>
+                    <p className='text-lg'>구로구 가산동</p>
+                    <p className='leading-6'>쓰리룸, 관리비 4만<br></br>혼자살기 좋은매물 준신축급<br></br>넓은1.5룸</p>
+                </div>
+            </div>
+        </div>
+        <div className="flex grow items-center w-full border_bottom_line p-[25px] bg-white h-[200]}">
+            <div className="bg-slate-600 border-black border-2">
+                <img height={200} width={160} src='/img/houseImg.jpeg' />
+            </div>
+            <div className="grow flex w-0 pl-5 ">
+                <div>
+                    <h1>[월세] 500/34</h1>
+                    <p className='text-lg'>구로구 가산동</p>
+                    <p className='leading-6'>쓰리룸, 관리비 4만<br></br>혼자살기 좋은매물 준신축급<br></br>넓은1.5룸</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+export {mapArr,mapArr2}
--- components/map/mapside.tsx
+++ components/map/mapside.tsx
@@ -1,119 +1,35 @@
-import HouseImage from '../../public/img/houseImg.jpeg';
-import HouseImage2 from '../../public/img/houseImg2.jpeg';
-import HouseImage3 from '../../public/img/houseImg3.jpeg';
-
+import {mapArr, mapArr2} from './mapArrData';
 import { Tabs } from "flowbite-react";
-import Image from 'next/image'
 
 
 const mapside = () => {
     const mapDataArr = [{
         id: 1,
-        content: <div className="w-full flex-col flex growcursor-pointer">
-            <div className="flex grow items-center w-full border_bottom_line p-[25px] bg-white h-[200]}">
-                <div className="bg-slate-600 border-black border-2">
-                    <Image width={160} height={200} src={HouseImage} />
-                </div>
-                <div className="grow flex w-0 pl-5 ">
-                    <div>
-                        <h1>[전세] 24억 1000</h1>
-                        <p className='text-lg'>구로구 가산동</p>
-                        <p className='leading-6'>쓰리룸, 관리비 4만<br></br>혼자살기 좋은매물 준신축급<br></br>넓은1.5룸</p>
-                    </div>
-                </div>
-            </div>
-            <div className="flex grow items-center w-full border_bottom_line p-[25px] bg-white h-[200]}">
-                <div className="bg-slate-600 border-black border-2">
-                    <Image height={200} width={160} src={HouseImage2} />
-                </div>
-                <div className="grow flex w-0 pl-5 ">
-                    <div>
-                        <h1>[매매] 4억 3000</h1>
-                        <p className='text-lg'>구로구 가산동</p>
-                        <p className='leading-6'>쓰리룸, 관리비 4만<br></br>혼자살기 좋은매물 준신축급<br></br>넓은1.5룸</p>
-                    </div>
-                </div>
-            </div>
-            <div className="flex grow items-center w-full border_bottom_line p-[25px] bg-white h-[200]}">
-                <div className="bg-slate-600 border-black border-2">
-                    <Image height={200} width={160} src={HouseImage3} />
-                </div>
-                <div className="grow flex w-0 pl-5 ">
-                    <div>
-                        <h1>[월세] 500/34</h1>
-                        <p className='text-lg'>구로구 가산동</p>
-                        <p className='leading-6'>쓰리룸, 관리비 4만<br></br>혼자살기 좋은매물 준신축급<br></br>넓은1.5룸</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        content: mapArr
     }]
 
     const mapDataArr2 = [{
         id: 2,
-        content: <div className="w-full flex-col flex growcursor-pointer ">
-            <div className="flex grow items-center w-full border_bottom_line p-[25px] bg-white h-[200]}">
-                <div className="bg-slate-600 border-black border-2">
-                    <img height={200} width={160} src='/img/houseImg2.jpeg' />
-
-                </div>
-                <div className="grow flex w-0 pl-5 ">
-                    <div>
-                        <h1>[매매] 4억 3000</h1>
-                        <p className='text-lg'>구로구 가산동</p>
-                        <p className='leading-6'>쓰리룸, 관리비 4만<br></br>혼자살기 좋은매물 준신축급<br></br>넓은1.5룸</p>
-                    </div>
-                </div>
-            </div>
-            <div className="flex grow items-center w-full border_bottom_line p-[25px] bg-white h-[200]}">
-                <div className="bg-slate-600 border-black border-2">
-                    <img height={200} width={160} src='/img/houseImg3.jpeg' />
-
-                </div>
-                <div className="grow flex w-0 pl-5 ">
-                    <div>
-                        <h1>[전세] 2억 1000</h1>
-                        <p className='text-lg'>구로구 가산동</p>
-                        <p className='leading-6'>쓰리룸, 관리비 4만<br></br>혼자살기 좋은매물 준신축급<br></br>넓은1.5룸</p>
-                    </div>
-                </div>
-            </div>
-            <div className="flex grow items-center w-full border_bottom_line p-[25px] bg-white h-[200]}">
-                <div className="bg-slate-600 border-black border-2">
-                    <img height={200} width={160} src='/img/houseImg.jpeg' />
-                </div>
-                <div className="grow flex w-0 pl-5 ">
-                    <div>
-                        <h1>[월세] 500/34</h1>
-                        <p className='text-lg'>구로구 가산동</p>
-                        <p className='leading-6'>쓰리룸, 관리비 4만<br></br>혼자살기 좋은매물 준신축급<br></br>넓은1.5룸</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        content: mapArr2
     }]
 
-    // const result = [
-    //     {id: 1, content: mapDataArr},
-    //     {id: 2, content: mapDataArr2}
-    // ];
-
     return (
-        <div className="overflow-hidden relative border-r-1 border-gray-300 h-full w-[600px] flex-initial flex flex-col">
+        <div className="overflow-hidden relative h-full w-[600px] flex-initial flex flex-col border-none">
             <Tabs.Group
                 aria-label="Tabs with underline"
                 style="underline"
             >
-                <Tabs.Item active={true} title="서울">
-                    {mapDataArr2.map((result) => {
-                        return (
-                            <div key={result.id}>
-                                <div>{result.content}</div>
-                            </div>
-                        );
-                    })}
-                </Tabs.Item>
-                <Tabs.Item active={true} title="중랑구">
+                    <Tabs.Item title="서울">
+                        {mapDataArr2.map((result) => {
+                            return (
+                                <div key={result.id}>
+                                    <div>{result.content}</div>
+                                </div>
+                            );
+                        })}
+                    </Tabs.Item>
+                
+                <Tabs.Item title="중랑구">
                     {mapDataArr.map((result) => {
                         return (
                             <div key={result.id}>
